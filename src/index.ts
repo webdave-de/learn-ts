9ee5dd0f6fc0cf28daf69ed13bd3c0765272ec9d
web-dave <dave@webdave.de>
2021-03-08 21:39:38 +0100

    canvas size

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-// Write TypeScript code!
+// Festergrösse
+const height = innerHeight -150;
+const width = innerWidth-5;
+
+// Leinwand
+const canvas = document.querySelector('canvas');
+canvas.height = height
+canvas.width = width
+const context = canvas.getContext('2d');
